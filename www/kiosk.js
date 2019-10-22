9424bf7af73b31b8aed647b69edeb2e79478eda2
Teagan glenn <teagan.m.glenn@gmail.com>
2019-10-21 18:37:48 -0600

    Kiosk css

--- www/kiosk.js
+++ www/kiosk.js
@@ -1,30 +1,44 @@
 if (window.location.href.indexOf('kiosk') > 0)
+    var style = document.createElement('style');
+    style.type = 'text/css';
+
+    var styles = [
+    'div#container {'+
+    '   margin: 0;' +
+    '}',
+    'app-drawer#drawer {' +
+    '   display: none'+
+    '}',
+    'app-headerr {' +
+    '   display: none'+
+    '}'];
+    document.getElementsByTagName('head')[0].appendChild(style);
     setTimeout(function () {
         try {
-            const root = document.querySelector('home-assistant').shadowRoot;
-            const main = root.querySelector('home-assistant-main').shadowRoot;
-            const drawer_layout = main.querySelector('app-drawer-layout');
-            const drawer = drawer_layout.querySelector('app-drawer');
-            drawer_sidebar = drawer.querySelector('ha-sidebar').shadowRoot;
-            const app_toolbar = drawer_sidebar.querySelector('app-toolbar');
-            const pages = drawer_layout.querySelector('partial-panel-resolver');
-            const lovelace = pages.querySelector('ha-panel-lovelace').shadowRoot;
-            const huiroot = lovelace.querySelector('hui-root').shadowRoot;
-            const header = huiroot.querySelector('app-header');
-            const toolbar = huiroot.querySelector('app-toolbar');
-            const button = toolbar.querySelector('paper-icon-button')
-            if (button) {
-                try {
-                    button.click();
-                } catch (e) {}
-            }
-            if (window.location.href.indexOf('show_tabs') > 0) {
-                toolbar.style.display = 'none';
-            } else {
-                header.style.display = 'none';
-            }
-            const mainView = huiroot.querySelector('div#view');
-            mainView.style.overflowY = 'hidden'
+            // const root = document.querySelector('home-assistant').shadowRoot;
+            // const main = root.querySelector('home-assistant-main').shadowRoot;
+            // const drawer_layout = main.querySelector('app-drawer-layout');
+            // const drawer = drawer_layout.querySelector('app-drawer');
+            // drawer_sidebar = drawer.querySelector('ha-sidebar').shadowRoot;
+            // const app_toolbar = drawer_sidebar.querySelector('app-toolbar');
+            // const pages = drawer_layout.querySelector('partial-panel-resolver');
+            // const lovelace = pages.querySelector('ha-panel-lovelace').shadowRoot;
+            // const huiroot = lovelace.querySelector('hui-root').shadowRoot;
+            // const header = huiroot.querySelector('app-header');
+            // const toolbar = huiroot.querySelector('app-toolbar');
+            // const button = toolbar.querySelector('paper-icon-button')
+            // if (button) {
+            //     try {
+            //         button.click();
+            //     } catch (e) {}
+            // }
+            // if (window.location.href.indexOf('show_tabs') > 0) {
+            //     toolbar.style.display = 'none';
+            // } else {
+            //     header.style.display = 'none';
+            // }
+            // const mainView = huiroot.querySelector('div#view');
+            // mainView.style.overflowY = 'hidden'
             window.dispatchEvent(new Event('resize'));
             if (!(self || {}).hassConnection) {
                 return;
