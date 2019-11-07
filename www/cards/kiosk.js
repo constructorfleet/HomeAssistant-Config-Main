4282f002d5d48de72ed051c77a171fabd37cf721
Teagan glenn <teagan.m.glenn@gmail.com>
2019-11-06 17:51:41 -0700

    put old kiosk

--- www/cards/kiosk.js
+++ www/cards/kiosk.js
@@ -17,30 +17,30 @@ if (window.location.href.indexOf('kiosk') > 0)
     window.dispatchEvent(new Event('resize'));
     setTimeout(function () {
         try {
-            // const root = document.querySelector('home-assistant').shadowRoot;
-            // const main = root.querySelector('home-assistant-main').shadowRoot;
-            // const drawer_layout = main.querySelector('app-drawer-layout');
-            // const drawer = drawer_layout.querySelector('app-drawer');
-            // drawer_sidebar = drawer.querySelector('ha-sidebar').shadowRoot;
-            // const app_toolbar = drawer_sidebar.querySelector('app-toolbar');
-            // const pages = drawer_layout.querySelector('partial-panel-resolver');
-            // const lovelace = pages.querySelector('ha-panel-lovelace').shadowRoot;
-            // const huiroot = lovelace.querySelector('hui-root').shadowRoot;
-            // const header = huiroot.querySelector('app-header');
-            // const toolbar = huiroot.querySelector('app-toolbar');
-            // const button = toolbar.querySelector('paper-icon-button')
-            // if (button) {
-            //     try {
-            //         button.click();
-            //     } catch (e) {}
-            // }
-            // if (window.location.href.indexOf('show_tabs') > 0) {
-            //     toolbar.style.display = 'none';
-            // } else {
-            //     header.style.display = 'none';
-            // }
-            // const mainView = huiroot.querySelector('div#view');
-            // mainView.style.overflowY = 'hidden'
+            const root = document.querySelector('home-assistant').shadowRoot;
+            const main = root.querySelector('home-assistant-main').shadowRoot;
+            const drawer_layout = main.querySelector('app-drawer-layout');
+            const drawer = drawer_layout.querySelector('app-drawer');
+            drawer_sidebar = drawer.querySelector('ha-sidebar').shadowRoot;
+            const app_toolbar = drawer_sidebar.querySelector('app-toolbar');
+            const pages = drawer_layout.querySelector('partial-panel-resolver');
+            const lovelace = pages.querySelector('ha-panel-lovelace').shadowRoot;
+            const huiroot = lovelace.querySelector('hui-root').shadowRoot;
+            const header = huiroot.querySelector('app-header');
+            const toolbar = huiroot.querySelector('app-toolbar');
+            const button = toolbar.querySelector('paper-icon-button')
+            if (button) {
+                try {
+                    button.click();
+                } catch (e) {}
+            }
+            if (window.location.href.indexOf('show_tabs') > 0) {
+                toolbar.style.display = 'none';
+            } else {
+                header.style.display = 'none';
+            }
+            const mainView = huiroot.querySelector('div#view');
+            mainView.style.overflowY = 'hidden'
             if (!(self || {}).hassConnection) {
                 return;
             }
