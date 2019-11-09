bf55a377c2c4c43ef5b6850d803f4e9b39b281ce
Teagan glenn <teagan.m.glenn@gmail.com>
2019-11-09 01:03:45 -0700

    Use deep query

--- www/cards/kiosk.js
+++ www/cards/kiosk.js
@@ -17,23 +17,29 @@ if (window.location.href.indexOf('kiosk') > 0) {
     window.dispatchEvent(new Event('resize'));
     setTimeout(function () {
         try {
-            const root = document.querySelector('home-assistant').shadowRoot;
-            const main = root.querySelector('home-assistant-main').shadowRoot;
-            const drawer_layout = main.querySelector('app-drawer-layout').shadowRoot;
-            const sidebar_slot = drawer_layout.querySelector('slot#drawerSlot').shadowRoot;
-            const drawer_content_container = drawer_layout.querySelector('div#contentContainer');
-            const drawer = main.querySelector('app-drawer-layout').querySelector('app-drawer');
+            // const root = document.querySelector('home-assistant').shadowRoot;
+            // const main = root.querySelector('home-assistant-main').shadowRoot;
+            // const drawer_layout = main.querySelector('app-drawer-layout').shadowRoot;
+            // const sidebar_slot = drawer_layout.querySelector('slot#drawerSlot').shadowRoot;
+            // const drawer_content_container = drawer_layout.querySelector('div#contentContainer');
+            // const drawer = main.querySelector('app-drawer-layout').querySelector('app-drawer');
+            // const drawer_sidebar = drawer.querySelector('ha-sidebar').shadowRoot;
+            // const app_toolbar = drawer_sidebar.querySelector('app-toolbar');
+            // const app_drawer_layout = main.querySelector('app-drawer-layout');
+            // const pages = app_drawer_layout.querySelector('partial-panel-resolver');
+            // const lovelace = pages.querySelector('ha-panel-lovelace').shadowRoot;
+            // const huiroot = lovelace.querySelector('hui-root').shadowRoot;
+            // const app_layout = huiroot.querySelector('ha-app-layout');
+            // const header = app_layout.querySelector('app-header');
+            // const toolbar = header.querySelector('app-toolbar');
+            // const button = toolbar.querySelector('paper-icon-button');
+            // const view_content_container = app_layout.shadowRoot.querySelector('div#wrapper').querySelector('div#contentContainer');
+
+
             const drawer_sidebar = drawer.querySelector('ha-sidebar').shadowRoot;
-            const app_toolbar = drawer_sidebar.querySelector('app-toolbar');
-            const app_drawer_layout = main.querySelector('app-drawer-layout');
-            const pages = app_drawer_layout.querySelector('partial-panel-resolver');
-            const lovelace = pages.querySelector('ha-panel-lovelace').shadowRoot;
-            const huiroot = lovelace.querySelector('hui-root').shadowRoot;
-            const app_layout = huiroot.querySelector('ha-app-layout');
-            const header = app_layout.querySelector('app-header');
-            const toolbar = header.querySelector('app-toolbar');
-            const button = toolbar.querySelector('paper-icon-button');
-            const view_content_container = app_layout.shadowRoot.querySelector('div#wrapper').querySelector('div#contentContainer');
+            const drawer = window.querySelectorDeep('app-drawer-layout > app-drawer')
+            const drawer_content_container = window.querySelectorDeep('app-drawer-layout > div#contentContainer');
+            const view_content_container = window.querySelectorDeep('div#contentContainer');
             drawer_sidebar.style = (drawer_sidebar.style || {});
             drawer.style = (drawer.style || {});
             drawer_content_container.style = (drawer_content_container.style || {});
@@ -44,11 +50,6 @@ if (window.location.href.indexOf('kiosk') > 0) {
             drawer_content_container.style.paddingTop = '0px';
             view_content_container.style.marginLeft = '0px';
             view_content_container.style.paddingTop = '0px';
-            // if (button) {
-            //     try {
-            //         button.click();
-            //     } catch (e) {}
-            // }
             if (window.location.href.indexOf('show_tabs') > 0) {
                 toolbar.style.display = 'none';
             } else {
