d4db4191ce80ba1a05f63a68df11528bb5fb683b
Teagan glenn <teagan.m.glenn@gmail.com>
2019-11-27 19:09:32 -0700

    Remove resize

--- www/cards/kiosk.js
+++ www/cards/kiosk.js
@@ -40,7 +40,6 @@ if (window.location.href.indexOf('kiosk') > 0) {
             if (!(self || {}).hassConnection) {
                 return;
             }
-            window.dispatchEvent(new Event('resize'));
             self.hassConnection.then(
                 function (hass) {
                     if (!(hass || {}).conn) { return; }
